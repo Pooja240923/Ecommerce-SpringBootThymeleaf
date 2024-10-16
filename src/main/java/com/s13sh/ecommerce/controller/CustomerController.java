7e0ce4a91effae6835634aaa5e059527148420f2
saishkulkarni <saishkulkarni7@gmail.com>
2024-10-16 10:53:36 +0530

    Seller Registration 1

---
+++ src/main/java/com/s13sh/ecommerce/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.s13sh.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@GetMapping("/register")
+	public String loadRegister() {
+		return "customer-register.html";
+	}
+}
